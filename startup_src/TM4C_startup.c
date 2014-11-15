05318e9e123bfac675dfa82e4fe645dad141d2ed
mukris <kristof.muki@mailbox.hu>
2014-11-15 23:37:22 +0100

    UART interrupt vektor beállítása

--- startup_src/TM4C_startup.c
+++ startup_src/TM4C_startup.c
@@ -50,6 +50,7 @@ extern void vPortSVCHandler(void);
 extern void xPortSysTickHandler(void);
 
 void ETH_IRQHandler(void) ALIAS(IntDefaultHandler);
+void UARTStdioIntHandler(void) ALIAS(IntDefaultHandler);
 
 //*****************************************************************************
 //
@@ -96,7 +97,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
-    IntDefaultHandler,                      // UART0 Rx and Tx
+	UARTStdioIntHandler,                    // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI0 Rx and Tx
     IntDefaultHandler,                      // I2C0 Master and Slave
