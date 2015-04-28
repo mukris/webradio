e10a07b88331f814f25214a326aedee48e9781c7
mukris <kristof.muki@mailbox.hu>
2015-04-28 02:17:42 +0200

    Heap méret növelése

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -75,7 +75,7 @@
 #define configCPU_CLOCK_HZ                  ( ( unsigned long ) 120000000 )
 #define configTICK_RATE_HZ                  ( 100 )
 #define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 200 )
-#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 60000 ) )
+#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 120000 ) )
 #define configMAX_TASK_NAME_LEN             ( 12 )
 #define configUSE_TRACE_FACILITY            1
 #define configUSE_16_BIT_TICKS              0
